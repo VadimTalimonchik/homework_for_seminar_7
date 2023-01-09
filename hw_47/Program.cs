6bff9d29b02cf82319518db8babb784fd58ad860
VadimTalimonchik <tvadmins@gmail.com>
2023-01-10 00:31:14 +0300

    Задача 47

--- hw_47/Program.cs
+++ hw_47/Program.cs
@@ -6,4 +6,41 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-Console.WriteLine("Hello, World!");
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Convert.ToDouble(new Random().Next(-100, 100))/10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальный диапазон массива: ");
+int minVol = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальный диапазон массива: ");
+int maxVol = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns, minVol, maxVol);
+PrintArray(array);
