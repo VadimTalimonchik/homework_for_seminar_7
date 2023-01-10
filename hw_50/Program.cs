854fe3ee0c50bb4b0fdb54a63383f0de7e9697a8
VadimTalimonchik <tvadmins@gmail.com>
2023-01-10 09:29:23 +0300

    Задача 50

--- hw_50/Program.cs
+++ hw_50/Program.cs
@@ -8,4 +8,47 @@
 // 8 4 2 4
 // [1,7] -> такого числа в массиве нет ([1,7] это позиция по индексам, 1- строка и 7 -столбец)
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите номер строки: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите номер столбца: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int [,] numbers = new int [3, 4];
+FillArray(numbers);
+
+if (m > numbers.GetLength(0) || n > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого числа в массиве нет!");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {m} строки и {n} столбца равно: {numbers[m-1, n-1]}");
+}
+Console.WriteLine();
+
+PrintArray(numbers);
+Console.WriteLine();
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }   
+        Console.WriteLine(""); 
+    }
+}
